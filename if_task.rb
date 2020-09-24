19bd66d5c83745ab53046ba37b3f8281aba18584
kurogi yuki <xtoriymoonz2@yahoo.co.jp>
2020-09-24 16:18:54 +0900

    add while

--- if_task.rb
+++ if_task.rb
@@ -12,26 +12,26 @@ end
 
 plan_number = gets.chomp.to_i
 
- if plan_number > 3
-   puts "存在しない番号です。１〜3の間で入力し直してください"
-   plan_number = gets.chomp.to_i
- end
+while plan_number > plans.size || plan_number == 0
+  puts "１〜3の間で入力し直してください"
+  plan_number = gets.chomp.to_i
+end
 
 plan_choice = plans[plan_number - 1]
 puts "#{plan_choice[:place]}ですね、何人で行きますか？"
 
 member = gets.chomp.to_i
 
-if member <= 0
- puts "人数は0人以上にしてください"
- member = gets.chomp.to_i
+while member == 0
+  puts "人数は0人以上にしてください"
+  member = gets.chomp.to_i
 end
 
 if member >= 5
   puts "五人以上なので10%割引となります"
-  sum = "合計金額:#{(plan_choice[:price] * member) * 90/100}"
+  total_price = (plan_choice[:price] * member) * 90/100
 else
-  sum = "合計金額:#{plan_choice[:price] * member}"
+  total_price = plan_choice[:price] * member
  end
 
-  puts sum
+  puts "合計金額: #{total_price}円"
