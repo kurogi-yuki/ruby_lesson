1455437481fc64fdf5b943c9d5171669883deade
kurogi yuki <xtoriymoonz2@yahoo.co.jp>
2020-09-25 14:27:44 +0900

    add_3

--- if_task.rb
+++ if_task.rb
@@ -12,8 +12,8 @@ end
 
 plan_number = gets.chomp.to_i
 
-while plan_number > plans.size || plan_number == 0
-  puts "１〜3の間で入力し直してください"
+while plan_number > plans.size || plan_number <= 0
+  puts "1〜#{plans.size}の間で入力し直してください"
   plan_number = gets.chomp.to_i
 end
 
@@ -22,7 +22,7 @@ puts "#{plan_choice[:place]}ですね、何人で行きますか？"
 
 member = gets.chomp.to_i
 
-while member == 0
+while member <= 0
   puts "人数は0人以上にしてください"
   member = gets.chomp.to_i
 end
